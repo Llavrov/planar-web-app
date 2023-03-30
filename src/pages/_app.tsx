98f4914aa2287a910d3c5b31d830edf48a849b82
ClockRide <a.dosta@icloud.com>
2023-03-30 06:57:39 -0700

    forcing the redeploy on vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import React from "react";
-import {Layout} from "@/ui/components/layout";
-import {NextUIProvider} from "@nextui-org/react";
+import { Layout } from "@/ui/components/layout";
+import { NextUIProvider } from "@nextui-org/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <main className="min-h-screen w-full">
         <section>
           <Layout>
-              <Component {...pageProps} />
+            <Component {...pageProps} />
           </Layout>
         </section>
       </main>
